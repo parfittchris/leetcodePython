c88107e87e84b85c4c24ad696abe679736188b25
Chris Parfitt <parfitt.chris7@gmail.com>
2020-07-04 11:05:36 -0400

    redo merge_sorted

--- array/merge_sorted_arr.py
+++ array/merge_sorted_arr.py
@@ -1,28 +1,41 @@
-def mergeSorted(arr1, arr2, m, n):
-    idx = len(arr1) - 1
+# Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
 
-    while idx >= 0:
+# Note:
+
+# The number of elements initialized in nums1 and nums2 are m and n respectively.
+# You may assume that nums1 has enough space(size that is equal to m + n) to hold additional elements from nums2.
+
+
+def mergeSorted(arr1, arr2, m, n):
+    i = len(arr1) - 1
+
+    while i >= 0:
         if m == 0:
-            arr1[idx] = arr2[n - 1]
+            next_val = arr2[n - 1]
             n -= 1
-        elif n == 0:
-            arr1[idx] = arr1[m-1]
-            m -= 1
-        elif arr1[m - 1] > arr2[n - 1]:
-            arr1[idx] = arr1[m-1]
+        elif n == 0:          
+            next_val = arr1[m - 1]
             m -= 1
         else:
-            arr1[idx] = arr2[n-1]
-            n -= 1
-        
-        idx -= 1
-        
-
+            if arr1[m - 1] > arr2[n - 1]:
+                next_val = arr1[m - 1]
+                m -= 1
+            else:
+                next_val = arr2[n - 1]
+                n -= 1
+            
+        arr1[i] = next_val
+        i -= 1
+    
     return arr1
 
-nums1 = [1, 2, 3, 0, 0, 0]
-m = 3
-nums2 = [2, 5, 6]
-n = 3
+
+nums1 = [-1, -1, 0, 0, 0, 0]
+
+m = 4
+nums2 = [-1, 0]
+
+n = 2
 
 print(mergeSorted(nums1, nums2, m, n))
+# Output: [1, 2, 2, 3, 5, 6]
--- array/search_in_rotated.py
+++ array/search_in_rotated.py
@@ -1,32 +1,18 @@
-# https: // leetcode.com/problems/search-in-rotated-sorted-array/
+# Suppose an array sorted in ascending order is rotated at some pivot unknown to you beforehand.
+
+# (i.e., [0, 1, 2, 4, 5, 6, 7] might become[4, 5, 6, 7, 0, 1, 2]).
+
+# You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+# You may assume no duplicate exists in the array.
+
+# Your algorithm's runtime complexity must be in the order of O(log n).
+
 
 def search(nums, target):
-    if len(nums) == 0:
-        return -1
+    
 
-    lo = 0
-    high = len(nums) - 1
-
-    while(lo <= high):
-        mid = (lo + high) / 2
-
-        if nums[mid] == target:
-            return mid
-
-        if nums[lo] <= nums[mid]:
-            if nums[lo] <= target <= nums[mid]:
-                high = mid - 1
-            else:
-                lo = mid + 1
-        else:
-            if nums[mid] <= target <= nums[high]:
-                lo = mid + 1
-            else:
-                high = mid -1
-
-    return -1
 
 nums = [4, 5, 6, 7, 0, 1, 2]
-target = 4
+target = 0
 
-print(search(nums, target))
